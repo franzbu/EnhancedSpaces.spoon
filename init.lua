5e66ea1c70508e7a4ebe32ebded3964243598965
franzbu <54638185+franzbu@users.noreply.github.com>
2024-11-28 14:55:55 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ EnhancedSpaces.author = "Franz B. <csaa6335@gmail.com>"
 EnhancedSpaces.homepage = "https://github.com/franzbu/EnhancedSpaces.spoon"
 EnhancedSpaces.license = "MIT"
 EnhancedSpaces.name = "EnhancedSpaces"
-EnhancedSpaces.version = "0.9.41.5"
+EnhancedSpaces.version = "0.9.41.6"
 EnhancedSpaces.spoonPath = scriptPath()
 
 local function tableToMap(table)
@@ -575,7 +575,7 @@ function mSpaceControl()
 
   local panSpace = currentMSpace
   for i = 1, #mSpaceControlShow do
-    hs.timer.doAfter(0.0000001, function()
+    --hs.timer.doAfter(0.0000001, function()
       goToSpace(indexOf(mspaces, mSpaceControlShow[i]))
       imgMSpaceControl[i] = hs.screen.mainScreen():snapshot(hs.geometry.new(0, heightMB, max.w, max.h)):setSize({w = max.w / 2, h = max.h / 2})
 
@@ -598,10 +598,10 @@ function mSpaceControl()
         baseCanvas:delete()
         frameCanvas:delete()
       end)
-    end)
+    --end)
   end
 
-  hs.timer.doAfter(0.0001, function()
+  --hs.timer.doAfter(0.0001, function()
     goToSpace(panSpace)
 
     baseCanvas = hs.canvas:new()
@@ -726,7 +726,7 @@ function mSpaceControl()
     end
     frameCanvas:show() -- show at the end, so frame is painted on top of adjacent mSpaces
     canvasMSpaceControl[currentMSpace]:show() -- necessary, otherwise frameCanvas would be on top
-  end)
+  --end)
 end
 
 
@@ -752,7 +752,7 @@ function refreshMenu()
     },
     { title = "-" },
     { title = menuTitles.help, fn = function() os.execute('/usr/bin/open https://github.com/franzbu/EnhancedSpaces.spoon/blob/main/README.md') end },
-    { title = menuTitles.about, fn =  function() hs.dialog.blockAlert('EnhancedSpaces', 'v0.9.41.5\n\n\nMakes you more productive.\nUse your time for what really matters.') end },
+    { title = menuTitles.about, fn =  function() hs.dialog.blockAlert('EnhancedSpaces', 'v0.9.41.6\n\n\nMakes you more productive.\nUse your time for what really matters.') end },
     { title = "-" },
     { title = hsTitle(), --image = hs.image.imageFromPath(hs.configdir .. '/Spoons/EnhancedSpaces.spoon/images/hs.png'):setSize({ h = 15, w = 15 }),
       menu = hsMenu(),
